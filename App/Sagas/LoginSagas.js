be70b145fb7fa60be01652b38e0c1934ffd81cd0
Leo Salat <leo.salat@gmail.com>
2017-04-03 23:06:43 -0700

    update loginUser to make api call

--- App/Sagas/LoginSagas.js
+++ App/Sagas/LoginSagas.js
@@ -2,12 +2,14 @@ import { put } from 'redux-saga/effects'
 import LoginActions from '../Redux/LoginRedux'
 
 // attempts to login
-export function * login ({ username, password }) {
-  if (password === '') {
-    // dispatch failure
-    yield put(LoginActions.loginFailure('WRONG'))
-  } else {
+export function * login (api, { username, password }) {
+  const response = yield call(api.loginUser, username, password)
+
+  if (response.ok) {
     // dispatch successful logins
-    yield put(LoginActions.loginSuccess(username))
+    yield put(LoginActions.loginSuccess(response.user))
+  } else {
+    // dispatch failure
+    yield put(LoginActions.loginFailure(response.errors))
   }
 }
