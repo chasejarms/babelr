de60cb1ed2620ae7f434557004c2dc1c3c480f46
Leo Salat <leo.salat@gmail.com>
2017-04-05 21:45:11 -0700

    update creators to match the api response

--- App/Sagas/SessionSagas.js
+++ App/Sagas/SessionSagas.js
@@ -2,27 +2,27 @@ import { put, call } from 'redux-saga/effects'
 import SessionActions from '../Redux/SessionRedux'
 
 // attempts to signup
-export function * signupUser (api, request) {
-  const response = yield call(api.signup, request)
+export function * signupUser (api, action) {
+  const response = yield call(api.signup, action.newUser)
 
   if (response.ok) {
     // dispatch successful signups
-    yield put(SessionActions.updateUser(response.user))
+    yield put(SessionActions.receiveCurrentUser(response.data.user))
   } else {
     // dispatch failure
-    yield put(SessionActions.receiveErrors(response.errors))
+    yield put(SessionActions.receiveErrors(response.data))
   }
 }
 
 // attempts to login
-export function * loginUser (api, userCredentials) {
-  const response = yield call(api.authorize, userCredentials)
+export function * loginUser (api, action) {
+  const response = yield call(api.authorize, action.userCredentials)
 
   if (response.ok) {
     // dispatch successful logins
-    yield put(SessionActions.loginSuccess(response.token))
+    yield put(SessionActions.loginSuccess(response.data.token))
   } else {
     // dispatch failure
-    yield put(SessionActions.receiveErrors(response.errors))
+    yield put(SessionActions.receiveErrors(response.data))
   }
 }
