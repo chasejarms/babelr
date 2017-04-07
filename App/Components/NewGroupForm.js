afb5d6f8f59aec4efa94206e2afbf9afa18c69f3
Chase Armstrong <chasejarms@gmail.com>
2017-04-06 22:54:14 -0700

    Add in inputs boxes and pill button to new form modal

--- App/Components/NewGroupForm.js
+++ App/Components/NewGroupForm.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import styles from './NewGroupForm'
-// import AuthTextInput from './AuthTextInput'
+import { View } from 'react-native'
+import styles from './Styles/NewGroupForm'
+import AuthTextInput from './AuthTextInput'
+import PillButton from './PillButton'
+import UserList from './UserList'
 
 export default class NewGroupForm extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      groupName: '',
+      userSearch: '',
+      selectedUsers: []
+    }
+  }
+
+  addUserToGroup = user => {
+    // TODO: check for users that are already in the array
+    // and don't show them on the search list
+    let newSelectedUsers = this.state.selectedUser.slice()
+    newSelectedUsers.push(user)
+    this.setState({
+      selectedUser: newSelectedUsers
+    })
+  }
+
+  update = field => text => {
+    this.setState({ [field]: text })
+  }
+
+  requestGroupCreation = () => {
+    // this function is merely a placeholder for now
+    console.log('nothing')
+  }
+
   render () {
     return (
       <View style={styles.formContainer}>
-        <Text>This is the new group form</Text>
+        <AuthTextInput
+          placeholder='your awesome group'
+          onChangeText={this.update('groupName')}
+          value={this.state.groupName}
+          />
+        <AuthTextInput
+          placeholder='search users by username'
+          onChangeText={this.update('userSearch')}
+          value={this.state.userSearch}
+          />
+        <UserList
+          addUserToGroup={this.addUserToGroup}
+          userSearch={this.state.userSearch} />
+        <PillButton
+          title='CREATE GROUP'
+          onPress={this.requestGroupCreation}
+          />
       </View>
     )
   }
