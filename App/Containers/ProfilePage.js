ba971dd2b54ba24b48e7d77f2af0c34b649d2884
Leo Salat <leo.salat@gmail.com>
2017-04-07 20:34:21 -0700

    add picker and additional info fields; build structure for styling

--- App/Containers/ProfilePage.js
+++ App/Containers/ProfilePage.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Picker } from 'react-native'
 import { Actions as NavigationActions } from 'react-native-router-flux'
 import SessionActions from '../Redux/SessionRedux'
 import PillButton from '../Components/PillButton'
 import styles from './Styles/ProfilePage'
 
 class ProfilePage extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      lang: 'en'
+    }
+  }
 
   handleLogout = () => {
     this.props.logout()
@@ -16,16 +22,70 @@ class ProfilePage extends Component {
   render () {
     return (
       <View style={styles.container}>
-        <View>
-          <Image
-            style={{width: 150, height: 150}}
-            source={{uri: 'https://s3-us-west-1.amazonaws.com/babelr/cb8.jpg'}} />
-          <Text style={styles.text}>"username"</Text>
-          <Text style={styles.text}>language dropdown</Text>
+        <View style={styles.userWrapper}>
+          <View style={styles.profile}>
+            <View style={styles.profileMain}>
+              <Image style={styles.avatar}
+                resizeMode={'cover'}
+                source={{uri: 'https://s3-us-west-1.amazonaws.com/babelr/cb8.jpg'}} />
+              <View style={styles.nameWrapper}>
+                <Text
+                  adjustsFontSizeToFit={false}
+                  style={styles.name}>
+                  Leo
+                </Text>
+                <Text
+                  adjustsFontSizeToFit={false}
+                  style={styles.name}>
+                  Salat
+                </Text>
+              </View>
+            </View>
+            <View style={styles.profileSub}>
+              <View style={styles.subTitleWrapper}>
+                <Text style={styles.subInfo}>username:</Text>
+                <Text style={styles.subInfo}>email:</Text>
+              </View>
+              <View style={styles.subInfoWrapper}>
+                <Text
+                  adjustsFontSizeToFit={false}
+                  style={styles.subInfo}>
+                  the_big_l
+                </Text>
+                <Text
+                  adjustsFontSizeToFit={false}
+                  style={styles.subInfo}>
+                  {'leo.salat@gmail.com'}
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.settings}>
+            <Text style={styles.settingTitle}>Preferred Language</Text>
+            <View style={styles.pickerContainer}>
+              <Picker
+                style={styles.picker}
+                itemStyle={styles.pickerItem}
+                selectedValue={this.state.lang}
+                onValueChange={(lang) => this.setState({lang})}>
+                <Picker.Item label='English' value='en' />
+                <Picker.Item label='Spanish' value='es' />
+                <Picker.Item label='Russian' value='ru' />
+                <Picker.Item label='Japanese' value='ja' />
+                <Picker.Item label='German' value='de' />
+                <Picker.Item label='Arabic' value='ar' />
+              </Picker>
+            </View>
+          </View>
         </View>
-        <PillButton
-          title='Logout'
-          onPress={this.handleLogout} />
+        <View style={styles.actions}>
+          <PillButton
+            title='Save'
+            onPress={this.handleSave} />
+          <PillButton
+            title='Logout'
+            onPress={this.handleLogout} />
+        </View>
       </View>
     )
   }
