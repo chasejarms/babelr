ca1f39c69cedbc6573ce30b25823582a07eab750
Leo Salat <leo.salat@gmail.com>
2017-04-07 20:37:04 -0700

    remove log statements

--- App/Sagas/SessionSagas.js
+++ App/Sagas/SessionSagas.js
@@ -17,7 +17,6 @@ export function * signupUser (api, action) {
 // attempts to login
 export function * loginUser (api, action) {
   const response = yield call(api.authorize, action.userCredentials)
-
   if (response.ok) {
     // dispatch successful logins
     api.config.setHeader('Authorization', `Token ${response.data.token}`)
@@ -30,8 +29,9 @@ export function * loginUser (api, action) {
 
 // request current user
 export function * requestUser (api, action) {
-  const response = yield call(api.requestUser, action.token)
+  const response = yield call(api.getUser, action.token)
 
+  console.tron.log(response)
   if (response.ok) {
     yield put(SessionActions.receiveCurrentUser(response.data.user))
   } else {
