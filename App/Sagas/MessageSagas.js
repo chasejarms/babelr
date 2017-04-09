08461216975d0a7eb9950be3c7740a489f45e2c5
Leo Salat <leo.salat@gmail.com>
2017-04-09 15:13:04 -0700

    update language argument to match api

--- App/Sagas/MessageSagas.js
+++ App/Sagas/MessageSagas.js
@@ -3,7 +3,7 @@ import MessageActions from '../Redux/MessageRedux'
 
 // attempts to get groups for a particular user, representing by their token
 export function * requestMessages (api, action) {
-  const response = yield call(api.requestMessages, action.groupId, action.language)
+  const response = yield call(api.requestMessages, action.groupId, `${action.language}_text`)
   if (response.ok) {
     // dispatch the relevant groups
     yield put(MessageActions.receiveMessages(response.data.messages))
