c51fce84589c23cb00366a0c027fe24412634d7d
Leo Salat <leo.salat@gmail.com>
2017-04-08 22:29:53 -0700

    remove token from group requests

--- App/Redux/GroupRedux.js
+++ App/Redux/GroupRedux.js
@@ -4,7 +4,7 @@ import Immutable from 'seamless-immutable'
 /* ------------- Types and Action Creators ------------- */
 
 const { Types, Creators } = createActions({
-  requestGroups: ['token'],
+  requestGroups: null,
   receiveGroups: ['groupItems'],
   receiveGroupErrors: ['errors'],
   logout: null
