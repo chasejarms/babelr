dfb4eb765d069431c73276d7ba1cff412e300dc4
Leo Salat <leo.salat@gmail.com>
2017-04-09 23:39:49 -0700

    set initial language

--- App/Containers/ProfilePage.js
+++ App/Containers/ProfilePage.js
@@ -10,7 +10,7 @@ class ProfilePage extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      preferredLanguage: 'en'
+      preferredLanguage: props.user.preferredLanguage
     }
   }
 
